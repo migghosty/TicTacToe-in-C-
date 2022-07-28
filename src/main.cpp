45ebabd228c42c427f365027e9ad2d8c86c545ed
migghosty <miguel6886@gmail.com>
2022-07-28 15:09:56 -0700

    Allow the user to pick the position they want to place their tile

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
 
-void printBoard(char arr[]) {
+void printBoard(char board[]) {
 	for (int i=0; i < 3; i++) {
 		for (int j=0; j < 3; j++) {
-			std::cout << " " << arr[i*3+j] << " ";
+			std::cout << " " << board[i*3+j] << " ";
 			if (j != 2) {
 				std::cout << "|";
 			}
@@ -25,14 +25,36 @@ void printBoard(char arr[]) {
 	}
 }
 
-int main() {
+int getPlayerPosition(int player, char board[]) {
+	int pos = 0;
 
+	std::cout << "Player " << player << ", enter a number: ";
+	std::cin >> pos;
+
+	while (pos < 1 || pos > 9 || board[pos-1] != (char)(pos+'0')) {
+		std::cout << "Invalid position" << std::endl;
+		std::cout << "Player " << player << ", enter a number: ";
+		std::cin >> pos;
+	}
+
+	return pos-1;
+}
+
+
+
+int main() {
 	char board[9];
 	for (int i=0; i < 9; i++) {
 		board[i] = (char)(i+'1');
 	}
+	int player = 1;
 
-	printBoard(board);
+	while (true) {
+		printBoard(board);
+		board[getPlayerPosition(player, board)] = (player == 1) ? 'x' : 'o';
+		player = (player == 1) ? 2 : 1;
+	}
+
 
 	return 0;
 }
